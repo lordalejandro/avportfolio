4f86799a5f76fe0db46b10aed492c96d39e5a4df
Alejandro Vilaca <alejandro.vilaca@itti.digital>
2023-02-14 16:39:17 -0300

    first

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
+import React from "react";
 
 function Intro() {
-   return (
-      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Forrest</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
-         <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm an indie full-stack developer and content creator building my
-            version of the digital world one step at a time. All coding projects
-            are built from the ground up, from planning and designing all the way
-            to solving real-life problems with code.
-            <br />
-            All video content is built the same way, from ideation and planning,
-            all the way to finalizing the content with artistic touches. I publish
-            that content on my YouTube channel{' '}
-            <a
-               href="https://youtube.com/fknight"
-               target="_blank"
-               className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
-               rel="noreferrer noopener"
-            >
-               ForrestKnight
-            </a>{' '}
-            to more than 400k subscribers.
-         </p>
-      </div>
-   )
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-14 pb-10">
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-light">
+        A. <span className="font-semibold">Vilaca</span>
+      </h1>
+      <p className="text-base md:text-xl mb-3 font-semibold">
+        Web Developer & Software Engineer
+      </p>
+      <p className="text-sm max-w-xl font-medium">
+        Full Stack Developer with almost 3 years of experience seeking a software
+        engineering role. With a strong foundation in JavaScript, React, and
+        Node.js, I have developed and maintained a variety of web applications,
+        from e-commerce sites to real-time collaboration tools. I am a quick
+        learner and have a passion for coding, continuously seeking out new
+        technologies and methodologies to improve my skills.
+        <br />
+        In my current role, I have demonstrated leadership by mentoring junior
+        developers and collaborating with cross-functional teams to deliver
+        high-quality products. I am excited to bring my technical expertise and
+        problem-solving skills to a dynamic software engineering team where I
+        can continue to grow and make an impact.
+      </p>
+    </div>
+  );
 }
 
 export default Intro;
